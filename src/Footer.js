b5613997f5b59e1b48bf6da162cd980c060cdf06
Naveen <kumarnav909@gmail.com>
2023-11-22 00:00:16 +0530

    lab5 done

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+// Footer.js
+import React from 'react';
+function MainContent() {
+  return (
+    <main>
+      <div>
+        <p>Copyright © 2023 My JSX App</p>
+        <p>Design</p>
+      </div>
+    </main>
+);
+}
+export default MainContent;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+// Header.js
+import React from 'react';
+function MainContent() {
+  return (
+    <main>
+        <div>
+          <h1>My JSX App Header</h1>
+          <p>My JSX App sub Header</p>
+        </div>
+    </main>
+ 
+);
+}
+export default MainContent;
